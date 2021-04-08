4fc49232eeeed0c4ceb97e85d953273da91f24e4
Arpad Gabor <hello@arpadgabor.com>
2021-04-09 01:54:05 +0300

    nuxt img for page preview

--- components/general/PagePreview.vue
+++ components/general/PagePreview.vue
@@ -32,7 +32,7 @@ export default defineComponent({
 <template>
   <Card v-if="page" as="article" class="bg-gray-50 dark:bg-gray-800 flex flex-col lg:flex-row space-y-8 lg:space-y-0 space-x-0 lg:space-x-8">
     <figure v-if="page.image" class="w-full lg:w-1/3 h-full">
-      <img :src="page.image.src" :alt="page.image.alt" class="object-cover rounded-lg h-full w-full">
+      <NuxtImg :src="page.image.src" :alt="page.image.alt" class="object-cover rounded-lg h-full w-full" fit="cover" />
     </figure>
 
     <div class="flex flex-col items-start justify-center w-full lg:w-2/3 space-y-4">
